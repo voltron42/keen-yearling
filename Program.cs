04e97c99710d31ab0ca1eeb2a25853cd19ad574e
Daniel Johnson <voltron42@users.noreply.github.com>
2023-04-04 21:21:48 +0000

    updating port to 80

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.Urls.Add("http://localhost:5000");
+app.Urls.Add("http://localhost:80");
 
 app.MapGet("/", () => "Hello World!");
 
